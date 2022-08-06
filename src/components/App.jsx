9bdd23419f9719ca5807d47a2bb03a920df6da02
Andriy Makarov <angeltek69@gmail.com>
2022-08-06 20:21:43 +0200

    Fullfield

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Section from './Section';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
